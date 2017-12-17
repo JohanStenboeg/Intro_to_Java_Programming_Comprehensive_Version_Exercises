c44657c1f73a050ae41b153c6d41e6de1f2ddd50
Johan <Joha6163@edu.easj.dk>
2017-12-17 17:35:14 +0100

    added eksamen1semesterpackage

---
+++ src/Eksame1semester/Task4.java
@@ -0,0 +1,17 @@
+package Eksame1semester;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+        int I = 0;
+        do {
+            System.out.println("Value of I: " + I);
+            I++;
+        }
+        while (I < 3);
+
+    }
+}
+
+
+
